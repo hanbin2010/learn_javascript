ec7103eed34dac197ea674a3a097675d7f012f5e
hanbin2010 <33389942+hanbin2010@users.noreply.github.com>
2017-11-06 23:37:52 +0900

    Add files via upload

---
+++ vibo/js/ball.js
@@ -0,0 +1,54 @@
+//js do not need import
+
+  var Ball = function (name) {
+    //imageFromPath関数を呼び出して、パスをその関数に渡して」、imageをゲットする
+    //imageはPaddleという形のオブジェクトの中で一つの属性である
+    // var image = imageFromPath('imgs/ball.png')
+      var o = game.imageByName('ball')
+    // oというオブジェクトがある
+    //自由にその中の属性を定義する
+    /*
+    var o = {
+      image:image,
+      x:100,
+      y:200,
+      //カンマを絶対つけたほうが良い
+      speedX:10,
+      speedY:10,
+      fired:false,
+    };
+    */
+
+    o.image=image
+    o.x=100
+    o.y=200
+    //カンマを絶対つけたほうが良い
+    o.speedX=10
+    o.speedY=10
+    o.fired=false
+
+
+    o.fire = function () {
+      o.fired = true
+
+    }
+  o.move = function () {
+      if(o.fired === true){
+        // console.log('move');
+        if(o.x < 0 || o.x > 400){
+          o.speedX = -o.speedX
+        }
+        if(o.y < 0 || o.y > 300){
+          o.speedY = -o.speedY
+        }
+        o.x += o.speedX
+        o.y += o.speedY
+      }
+    }
+
+    o.rebound = function () {
+      o.speedY *= -1
+    }
+    //いろんな属性とかを持つオブジェクトにする
+    return o
+  }
---
+++ vibo/js/main.js
@@ -0,0 +1,155 @@
+//js do not need import
+
+
+//dirty code
+//change paused and blocks to globlal var
+window.paused = false
+
+var loadLevel = function (n,game) {
+  n = n -1
+  //one element in array
+  var level = levels[n]
+  //empty array
+  var blocks = []
+  //create blocks by level
+  for (var i = 0; i < level.length; i++) {
+    //p is position
+    var p = level[i]
+    //call Block func and pass p to block
+    var b = Block(p,game)
+    //add an element
+    blocks.push(b)
+  }
+  return blocks
+}
+
+
+//デバッグ用
+var enableDebugMode = function (flg,game) {
+  if(!flg){
+    return
+  }
+
+// so what is the difference key up vs key down??
+  window.addEventListener('keyup',function () {
+    //use short var name in the func
+    var k = event.key
+    //一時停止機能
+      if(k === 'p'){
+        //change paused
+        window.paused = !window.paused
+      }else if ('1234567'.includes(k)) {
+            window.blocks = loadLevel(Number(k),game)
+      }
+    //   }else if (event.key === '1') {
+    //     blocks = loadLevel(1)
+    // }else if (event.key === '2') {
+    //     blocks = loadLevel(2)
+    //   }
+  })
+
+  //speed control
+  //too much in event
+  //イベントの中にいろんなものがありすぎて、わかりにくい
+  document.querySelector('#id-input-speed').addEventListener('input',function (event) {
+    //event.targetは、イベントバブルのイベントを開始したDOM要素を返すプロパティ。
+    // イベントバブルとは、子要素のイベントが発生すると、
+    //その親要素や先祖要素の同じタイプのイベントが発生すること。
+    var input = event.target
+    window.fps = Number(input.value)
+  })
+}
+
+//入り口を一つにする
+var __main = function(){
+
+  //define var
+  var images = {
+        block:'imgs/block.png',
+        ball:'imgs/ball.png',
+        paddle:'imgs/paddle.png',
+  }
+    var game = GuaGame(30,images)
+    var paddle = Paddle(game)
+    var ball = Ball(game)
+    var score = 0
+    
+    //call func
+    //set debug mode true
+    enableDebugMode(true,game)
+    // window.blocks = loadLevel(1,game)
+    var blocks = loadLevel(1,game)
+
+    //paddleと関係ないことも関数に入れる
+    //canvasを描画する
+    //そうすると、画面がロードされたときに、自動的にcanvasの中に出てくる
+    paddle.image.onload = function() {
+      game.context.drawImage(paddle.image, paddle.x, paddle.y)
+    }
+
+    //events
+    game.registerAction('a',function () {
+      // console.log("もしキーが押されたら、登録されたactionを実施");
+      paddle.moveLeft()
+    })
+
+    game.registerAction('d',function () {
+      paddle.moveRight()
+    })
+
+    game.registerAction('f',function () {
+      ball.fire()
+    })
+
+    // game.registerAction('p',function () {
+    // })
+
+
+//drawとupdateは本来ならguaGameの中にいるはず
+//今はpaddleとかballとか一緒に使っているんで、とても分かりにくい
+    //draw
+    game.draw = function () {
+      // console.log("draw");
+      game.drawImage(paddle)
+      game.drawImage(ball)
+
+      for (var i = 0; i < window.blocks.length; i++) {
+        var b = window.blocks[i]
+        if(b.alive){
+          game.drawImage(b)
+        }
+      }
+
+      //draw lables
+      game.context.fillText("score is "+score, 10, 290)
+    }
+
+
+    game.update = function () {
+      if(window.paused){
+        // console.log("stop");
+        return
+      }
+      // console.log('update');
+      ball.move()
+      //collide
+      if(paddle.collide(ball)){
+        //ここはball.リバウンド()関数を呼び出すべき　
+          ball.rebound()
+        // ball.speedY *= -1
+      }
+      for (var i = 0; i < window.blocks.length; i++) {
+        var b = window.blocks[i]
+        if(b.collide(ball)){
+          console.log("ball collide");
+          b.kill()
+          ball.rebound()
+          //update score
+          score = score + 100
+        }
+      }
+    }
+}
+
+//start main function
+__main()
